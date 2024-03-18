ee4b611699f918483c8bd324e51dad85ea532ab0
tho888 <105895875+tho888@users.noreply.github.com>
2024-03-18 14:32:13 +1300

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,7 +1,3 @@
-// function preload() {
-//   background(10);
-// }
-
 function draw_clock(obj) {
   // draw your own clock here based on the values of obj:
   //    obj.hours goes from 0-23
@@ -12,37 +8,38 @@ function draw_clock(obj) {
   //        < 0 if no alarm is set
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
-  
+
   // -------------------------- setup --------------------------
   angleMode(DEGREES);
-  // background(10);
+  background(24);
   fill(255);
   textSize(10);
 
   // -------------------------- text clock indicators --------------------------
-  // fill(175); 
-  // text("Hour: "   + obj.hours, 10, 20);
-  // text("Minute: " + obj.minutes, 10, 40);
-  // text("Second: " + obj.seconds, 10, 60);
-  // text("Millis: " + obj.millis, 10, 80);
+  fill(175);
+  text("Hour: " + obj.hours, 10, 20);
+  text("Minute: " + obj.minutes, 10, 40);
+  text("Second: " + obj.seconds, 10, 60);
+  text("Millis: " + obj.millis, 10, 80);
 }
 
 function spirograph() {
   // -------------------------- main clock --------------------------
   translate(width / 2, height / 2);
-  fill(231, 231, 231, 255);
-  strokeWeight(2);
+  noStroke();
 
-  rotate(360 / 60 * obj.minutes - 90);
-  translate(100, 0);
+  let millisMap = map(obj.millis, 0, 999, 0, 500);
+  let secondsMap = map(obj.seconds, 0, 59, 0, 500);
+  let minutesMap = map(obj.minutes, 0, 59, 0, 500);
+  let hoursMap = map(obj.hours, 0, 23, 0, 500);
 
-  rotate(360 / 60 * obj.seconds - 90);
-  translate(50, 0);
+  fill('rgba(231, 0, 0, 0.2)');
+  ellipse(0, 0, millisMap, millisMap);
+  fill('rgba(0, 231, 0, 0.2)');
+  ellipse(0, 0, secondsMap, secondsMap);
+  fill('rgba(0, 0, 231, 0.2)');
+  ellipse(0, 0, minutesMap, minutesMap);
+  fill('rgba(231, 231, 231, 0.2)');
+  ellipse(0, 0, hoursMap, hoursMap);
 
-  rotate(360 / 1000 * obj.millis);
-  translate(10, 0);
-
-  point(0,0);
 }
-
-// use preload() to change background once
