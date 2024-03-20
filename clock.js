9fc4318b402437f49afc56b48a059f5b6b9180a3
tho888 <105895875+tho888@users.noreply.github.com>
2024-03-20 16:59:51 +1300

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,36 +1,52 @@
 function draw_clock(obj) {
 
   // ____________________ setup & variables ____________________
-  let secondsWidth = map(obj.seconds, 0, 59, 0, 25);
-  let minutesWidth = map(obj.minutes, 0, 59, 0, 25);
-  let twelveHourTime = obj.hours % 12;
-  let hoursWidth = map(twelveHourTime, 0, 11, 0, 25);
-  // let secondsWithFraction = obj.seconds + (obj.millis / 1000.0);
-  // let secondHandRotation = map(secondsWithFraction, 0, 60, 0, 360);
-
+  angleMode(DEGREES);
+  colorMode(HSB);
+  fill(255);
+  textSize(10);
+  noStroke();
+  
   let clockSize = 210;
   let minutesScale = 1.75;
   let hoursScale = 3.5;
 
-  let bgColour = "rgb(20, 20, 20)"
-  let secondsColour = "rgb(101, 101, 101)";
-  let minutesColour = "rgb(141, 141, 141)";
-  let hoursColour = "rgb(181, 181, 181)";
-  
-  angleMode(DEGREES);
-  background(bgColour);
-  fill(255);
-  textSize(10);
-  noStroke();
+  let bgColour = 0;
+  let bgColourAlarm = 75;
+// add a map to change background colour gradually as timer counts down
+// add more maps '' for seconds, hours and minutes
+
+  let secondsColour = 50;
+  let secondsColourAlarm = 10;
+  let minutesColour = 70;
+  let minutesColourAlarm = 30;
+  let hoursColour = 90;
+  let hoursColourAlarm = 50;
+
+  if (obj.seconds_until_alarm < 0) {
+    background(bgColour);
+
+  } else if (obj.seconds_until_alarm > 0) {
+    background(bgColourAlarm); // change this to the created map
+
+  } else if (obj.seconds_until_alarm == 0) {
+    if (obj.seconds % 2 === 0) {
+      background(bgColour);
+
+    } else {
+      background(bgColourAlarm);
+      
+    }
+  }
 
   // ____________________ debug text time ____________________
-  push();
-  textAlign(LEFT);
-  text("Hour: " + obj.hours, 10, 20);
-  text("Minute: " + obj.minutes, 10, 40);
-  text("Second: " + obj.seconds, 10, 60);
-  text("Millis: " + obj.millis, 10, 80);
-  pop();
+  // push();
+  // textAlign(LEFT);
+  // text("Hour: " + obj.hours, 10, 20);
+  // text("Minute: " + obj.minutes, 10, 40);
+  // text("Second: " + obj.seconds, 10, 60);
+  // text("Millis: " + obj.millis, 10, 80);
+  // pop();
 
   // ____________________ main clock ____________________
 
@@ -42,7 +58,21 @@ function draw_clock(obj) {
   push();
   for (i = 0; i < 60; i++) {
     rotate(6);
-    stroke(secondsColour);
+
+    if (obj.seconds_until_alarm < 0) {
+      stroke(secondsColour);
+
+    } else if (obj.seconds_until_alarm == 0) {
+
+      if (obj.seconds % 2 === 0) {
+        stroke(secondsColour);
+
+      } else {
+        stroke(secondsColourAlarm);
+
+      }
+    }
+
     point(-clockSize, 0);
   }
   pop();
@@ -50,7 +80,21 @@ function draw_clock(obj) {
   push();
   for (j = 0; j < 60; j++) {
     rotate(6);
-    stroke(minutesColour);
+ 
+    if (obj.seconds_until_alarm < 0) {
+      stroke(minutesColour);
+
+    } else if (obj.seconds_until_alarm == 0) {
+    
+      if (obj.seconds % 2 === 0) {
+        stroke(minutesColour);
+
+      } else {
+        stroke(minutesColourAlarm);
+
+      }
+    }
+
     point(-clockSize / minutesScale, 0);
   }
   pop();
@@ -58,24 +102,51 @@ function draw_clock(obj) {
   push();
   for (k = 0; k < 12; k++) {
     rotate(30);
-    stroke(hoursColour);
+
+    if (obj.seconds_until_alarm < 0) {
+      stroke(hoursColour);
+
+    } else if (obj.seconds_until_alarm == 0) {
+    
+      if (obj.seconds % 2 === 0) {
+        stroke(hoursColour);
+
+      } else {
+        stroke(hoursColourAlarm);
+
+      }
+    }
+
     point(-clockSize / hoursScale, 0);
   }
   pop();
 
-
-
+  // ____________________ hands / numbers ____________________ 
   strokeWeight(1);
   textAlign(CENTER);
 
-  // ____________________ hands / numbers ____________________ 
   push();
   rotate(6 * obj.seconds);
-  fill(secondsColour);
-  stroke(secondsColour);
+  
+  if (obj.seconds_until_alarm < 0) {
+    fill(secondsColour);
+    stroke(secondsColour);
+
+  } else if (obj.seconds_until_alarm == 0) {
+
+    if (obj.seconds % 2 === 0) {
+      fill(secondsColour);
+      stroke(secondsColour);
+
+    } else {
+      fill(secondsColourAlarm);
+      stroke(secondsColourAlarm);
+
+    }
+  }
+  
   textSize(clockSize / 16)
   text(obj.seconds, 0, -clockSize / 10 - clockSize);
-  
   rotate(90);
   line(-clockSize, 0, clockSize, 0);
   pop();
@@ -83,23 +154,53 @@ function draw_clock(obj) {
 
   push();
   rotate(6 * obj.minutes);
-  fill(minutesColour);
-  stroke(minutesColour);
+
+  if (obj.seconds_until_alarm < 0) {
+    fill(minutesColour);
+    stroke(minutesColour);
+
+  } else if (obj.seconds_until_alarm == 0) {
+
+    if (obj.seconds % 2 === 0) {
+      fill(minutesColour);
+      stroke(minutesColour);
+
+    } else {
+      fill(minutesColourAlarm);
+      stroke(minutesColourAlarm);
+
+    }
+  }
+
   textSize(clockSize / 16)
   text(obj.minutes, 0, -clockSize / 10 - clockSize / minutesScale);
-  
   rotate(90);
   line(-clockSize / minutesScale, 0, clockSize / minutesScale, 0);
   pop();
-
+  
 
   push();
   rotate(30 * obj.hours);
-  fill(hoursColour);
-  stroke(hoursColour);
+
+  if (obj.seconds_until_alarm < 0) {
+    fill(hoursColour);
+    stroke(hoursColour);
+
+  } else if (obj.seconds_until_alarm == 0) {
+
+    if (obj.seconds % 2 === 0) {
+      fill(hoursColour);
+      stroke(hoursColour);
+
+    } else {
+      fill(hoursColourAlarm);
+      stroke(hoursColourAlarm);
+
+    }
+  }
+
   textSize(clockSize / 16)
   text(obj.hours % 12, 0, -clockSize / 10 - clockSize / hoursScale);
-  
   rotate(90);
   line(-clockSize / hoursScale, 0, clockSize / hoursScale, 0);
   pop();
